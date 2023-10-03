283c53bbc9b682386bbab65b473a34754fa39752
Connor Adams <connorvadams@gmail.com>
2023-10-03 07:13:28 -0600

    Submits lab.

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,14 +4,14 @@ function Item({ name, category }) {
   const [inCart, setInCart] = useState("")
 
   function toggleInCart() {
-    setInCart((!inCart) ? "in-cart" : "")
+    setInCart(!inCart ? "in-cart" : "")
   }
 
   return (
     <li className={inCart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add" onClick={toggleInCart}>Add to Cart</button>
+      <button className="add" onClick={toggleInCart}>{!inCart ? "Add to Cart" : "Remove From Cart"}</button>
     </li>
   );
 }
